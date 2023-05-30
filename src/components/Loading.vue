5de620b7e1022b03996808e5f7c85ca176321251
KayLin <kay94837@gmail.com>
2023-05-30 17:52:21 +0800

    modify: change style

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -25,15 +25,13 @@ const loading = useLoadingStore()
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100%;
-  z-index: 9999;
-  background-color: rgba(0, 0, 0, 0.3);
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.3);
 }
 
 .lds-roller {
